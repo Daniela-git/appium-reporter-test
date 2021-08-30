0734be958eefa610741ae5da797c1471d6e32e23
Daniela <dhiguitaa17@gmail.com>
2021-08-30 18:40:28 -0500

    change in the error info

--- reporter/custom.reporter.ts
+++ reporter/custom.reporter.ts
@@ -4,6 +4,21 @@ import WDIOReporter, {
   TestStats,
 } from '@wdio/reporter';
 
+const agent = require('superagent');
+const userName = 'Daniela-git';
+const repoName = 'appium-reporter-test';
+const urlBase = 'https://api.github.com';
+const url = `${urlBase}/repos/${userName}/${repoName}/issues`;
+
+interface dataToShare {
+  uid: string;
+  suitTitle: string;
+  testTitle: string;
+  state: string;
+  retries: number;
+  error: string | undefined;
+  file: string;
+}
 module.exports = class CustomReporter extends WDIOReporter {
   constructor(options: any) {
     super(options);
@@ -16,17 +31,34 @@ module.exports = class CustomReporter extends WDIOReporter {
     console.log(`${test.title} HAS AN ERROR (T.T)`);
     console.log(`This is the error ${test.error}`);
   }
-  onTestEnd(test: TestStats) {
-    this.write(`\n ${test.title} ------- ${test.state}`);
-  }
-  onSuiteEnd(suit: SuiteStats) {
-    console.log(suit.title);
-    suit.tests.forEach((test) => {
-      console.log(`${test.title}-----${test.state}`);
+  async onSuiteEnd(suit: SuiteStats) {
+    suit.tests.forEach(async (test) => {
+      const data: dataToShare = {
+        uid: suit.uid,
+        suitTitle: suit.title,
+        testTitle: test.title,
+        state: test.state,
+        retries: test.retries,
+        error: undefined,
+        file: suit.file,
+      };
+      if (test.error != undefined) {
+        data.error = test.error.stack;
+      }
+
+      await this.sendTestResults(url, data);
     });
   }
-  onRunnerEnd(runner: RunnerStats) {
-    console.log(runner.specs);
-    console.log(runner.failures);
+
+  async sendTestResults(url: string, test: dataToShare) {
+    await agent
+      .post(url)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .set('User-Agent', 'agent')
+      .send({
+        title: `${test.suitTitle} ${test.testTitle}`,
+        body: `uid: ${test.uid}, \n suit title: ${test.suitTitle}, \n test title: ${test.testTitle},\n state: ${test.state}, \n retries: ${test.state}, \n file: ${test.file}, \n error: ${test.error}`,
+      })
+      .then((res) => console.log(res.status));
   }
 };
